24d570d5ac1efd57de35062242a231a79d3de11a
Dustin Crossman <dustin.crossman@gmail.com>
2013-12-03 22:22:53 -0800

    Made ROS workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/dustin/programming/proto2/src
+# Build directory: /home/dustin/programming/proto2/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,4 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/dustin/programming/proto2/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
