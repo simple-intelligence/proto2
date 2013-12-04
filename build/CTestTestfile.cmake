f3609506416f2417085cd69a92cf683429d085f1
Dustin Crossman <dustin.crossman@gmail.com>
2013-12-03 23:00:02 -0800

    Cleaned up plan branch

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dustin/programming/proto2/src
-# Build directory: /home/dustin/programming/proto2/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
