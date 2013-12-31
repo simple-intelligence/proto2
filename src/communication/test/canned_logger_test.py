883dcaa04b7b4716fbf6f16d2b70d34c93defbb4
Dustin Crossman <dustin.crossman@gmail.com>
2013-12-31 00:33:31 -0800

    Logger test no longer outputs to file. (Just cleaning things up)

--- src/communication/test/canned_logger_test.py
+++ src/communication/test/canned_logger_test.py
@@ -9,16 +9,16 @@ from zmq_communicator import communicator
 def main ():
 	com = communicator ("Logger", "canned_communication_settings.json")
 	
-	logger = open ("proto2.log", 'w')
+	#logger = open ("proto2.log", 'w')
 
 	while True:
 		for module in com.get_listening_to ():
 			msg = com.get_message (module)
 			print msg
-			logger.write (str (msg) + '\n')
+			#logger.write (str (msg) + '\n')
 		sleep (1)
 
-	logger.close ()
+	#logger.close ()
 
 
 main ()
