c81f00f4521fd8eb4dd31a1c57fe5b61c885dce0
Dustin Crossman <dustin.crossman@gmail.com>
2014-01-30 23:01:48 -0800

    Improved video reciever

--- src/vision/video_reciever.py
+++ src/vision/video_reciever.py
@@ -7,7 +7,7 @@ sys.path.append (os.path.abspath ("../../"))
 from src.communication.zmq_communicator import communicator
 from src.communication.network_utils import video_reciever
 
-class Vision_Processor ():
+class Vision_Processor:
 	def __init__(self):
 		self.images = {}
 	
@@ -15,19 +15,27 @@ class Vision_Processor ():
 
 		self.outputs = {}
 
-		self.outputted_images = []
+		self.outputted_images = ["H_Thresh_Blur", "Src"]
 	
 	def process_image (self, image):
 		self.images["Src"] = deepcopy (image) 
 
 		self.images["HSV"] = cv2.cvtColor (self.images["Src"], cv2.COLOR_BGR2HSV)
 
-		self.images["H"], a, b = cv2.split (self.images["HSV"])
+		self.images["H"], self.images["S"], self.images["V"] = cv2.split (self.images["HSV"])
 
-		self.images["H_Thresh"] = cv2.threshold (self.images["H"], 100, 255, cv2.THRESH_BINARY)
+		ret, self.images["H_Thresh"] = cv2.threshold (self.images["H"], 100, 255, cv2.THRESH_BINARY)
 
-		ret, self.images["H_Thresh_Blur"] = cv2.medianBlur (self.images["H_Thresh"], 9)
+		self.images["H_Thresh_Blur"] = cv2.medianBlur (self.images["H_Thresh"], 9)
 
+		self.outputs["Circles"] = cv2.HoughCircles (self.images["H_Thresh_Blur"], cv2.cv.CV_HOUGH_GRADIENT, 2, 100) #, 200, 120)
+
+		try:
+			for i in self.outputs["Circles"][0]:
+				cv2.circle (self.images["Src"], (i[0], i[1]), i[2], (0, 0, 255), thickness=2)
+				print "X: {x}, Y: {y}".format (x=i[0], y=i[1])
+		except:
+			pass
 
 	def show_images (self):
 		if self.outputted_images:
@@ -45,18 +53,17 @@ class Vision_Processor ():
 
 
 def main ():
-	#processor = Vision_Processor ()
+	processor = Vision_Processor ()
 	reciever = video_reciever ("Downward")
 
-	reciever.ready_up () # This is necessary since zmq will always drop the first (at least) message. This is the initial connect
+	#reciever.ready_up () # This is necessary since zmq will always drop the first (at least) message. This is the initial connect
 	while True:
 		frame = reciever.get_frame ()
 
 		if frame is not None:
 			try:
-				#processor.process_image (frame)
-				#processor.show_images ()
-				cv2.imshow ("frame", frame)
+				processor.process_image (frame)
+				processor.show_images ()
 			except:
 				pass
 
