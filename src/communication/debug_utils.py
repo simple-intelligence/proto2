c39fd37275092248ccea703138d16448f8562951
Dustin Crossman <dustin.crossman@gmail.com>
2014-01-30 17:24:48 -0800

    Fixed print_d in debugger

--- src/communication/debug_utils.py
+++ src/communication/debug_utils.py
@@ -4,11 +4,15 @@ import json
 class debugging:
 	def __init__(self):
 		try:
-			settings = json.load (open ("/home/dustin/programming/ros_workspace/src/proto2/Global_Settings.json", "r"))
+			self.settings = json.load (open ("/home/dustin/programming/ros_workspace/src/proto2/Global_Settings.json", "r"))
 		except:
 			sys.stderr.write ("Global_Settings.json does not exist or is not in json format!\n")
 			sys.exit ()
 
-		def print_d (self, msg):
-			if settings["Debug"]:
-				sys.stderr.write (str (msg) + "\n")
+		if self.settings["Debug"]:
+			print "DEBUG = True"
+
+	def print_d (self, msg):
+		if self.settings["Debug"]:
+			sys.stderr.write (str (msg) + "\n")
+			print str (msg)
